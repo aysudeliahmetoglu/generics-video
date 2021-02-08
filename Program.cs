25733aa80e51bbb6fb51055f91c723776ad8b51a
aysudeliahmetoglu <76439892+aysudeliahmetoglu@users.noreply.github.com>
2021-02-08 23:21:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();// turkuaz olanlar class
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            Console.WriteLine(sehirler.Count);
+
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            Console.WriteLine(sehirler2.Count);
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+        
+        public MyList ()
+            {
+            _array = new T[0];
+            }
+
+     
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_tempArray.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+      
+
+        public int Count
+        {
+            get { return _array.Length; }
+           
+        }
+
+    }
+}
